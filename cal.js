da16ed9260462f00b7ca52adb97ef80294ba21d0
jayashreebalinani <jayashreebalinani@gmail.com>
2023-03-02 22:21:10 +0530

    update4

---
+++ cal.js
@@ -0,0 +1,117 @@
+function display7()
+{
+    var data =document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"7"
+   
+}
+function display8()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"8"
+   
+}
+function display9()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"9"
+   
+}
+function display6()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"6"
+    
+}
+function display5()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"5"
+    
+}
+function display4()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"4"
+    
+}
+function display3()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"3"
+    
+}
+function display2()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"2"
+    
+}
+function display1()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"1"
+   
+}
+function display0()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"0"
+    
+}
+function neg()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText="-"+data
+
+}
+function sqr()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data**2
+}
+function displaydot()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"."
+   
+}
+function displayplus()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"+"
+   
+    
+}
+function clear()
+{
+    var data = document.getElementById("input").innerText
+   document.getElementById("input").innerText=data
+   document.append.clear
+   
+    
+}
+function displayminus()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"-"
+   
+}
+function displaymul()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"*"
+    
+
+}
+function displaymod()
+{
+    var data = document.getElementById("input").innerText
+    document.getElementById("input").innerText=data+"%"
+    
+
+}
+function solve()
+{
+    var data =eval(document.getElementById("input").innerText)
+    document.getElementById("input").innerText=data
+}
